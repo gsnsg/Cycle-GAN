9450a2e51e20a7fbf0bf8c111b77bdba6d759ca2
Sai Nikhit Gulla <sainikhit2k@gmail.com>
2020-07-05 23:22:50 +0530

    Update models.py

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import *
 from keras.layers import *
+from keras.optimizers import Adam
 from layers import *
 
 # Here we will be using "same" padding  rather than reflected padding
@@ -61,19 +62,25 @@ def define_generator(inp_shape, num_resnet_blocks, summary=True):
 def define_discriminator(inp_shape, summary=False):
     inp = Input(shape=inp_shape)
     kernel_init = tf.keras.initializers.TruncatedNormal(stddev=0.02)
-    h0 = Conv2D(32, (4, 4), bias_initializer=None, kernel_initializer=kernel_init, strides=2)(inp)
-    h0 = LeakyReLU()(h0)
-    h1 = Conv2D(64, (4, 4), bias_initializer=None, kernel_initializer=kernel_init, strides=2)(h0)
-    h1 = BatchNormalization()(h1)
-    h1 = LeakyReLU()(h1)
-    h2 = Conv2D(128, (4, 4), bias_initializer=None, kernel_initializer=kernel_init, strides=2)(h1)
-    h2 = BatchNormalization()(h2)
-    h2 = LeakyReLU()(h2)
-    h3 = Conv2D(256, (4, 4), bias_initializer=None, kernel_initializer=kernel_init, strides=2)(h2)
-    h3 = BatchNormalization()(h3)
-    h3 = LeakyReLU()(h3)
-    h4 = Conv2D(1, (3, 3), bias_initializer=None, kernel_initializer=kernel_init)(h3)
-    model = Model(inputs=[inp], outputs=[h4])
+    d = Conv2D(64, (4,4), strides=(2,2), padding='same', kernel_initializer=kernel_init)(inp)
+    d = LeakyReLU(alpha=0.2)(d)
+    d = Conv2D(128, (4,4), strides=(2,2), padding='same', kernel_initializer=kernel_init)(d)
+    d = InstanceNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+    d = Conv2D(256, (4,4), strides=(2,2), padding='same', kernel_initializer=kernel_init)(d)
+    d = InstanceNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+    d = Conv2D(512, (4,4), strides=(2,2), padding='same', kernel_initializer=kernel_init)(d)
+    d = InstanceNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+    d = Conv2D(512, (4,4), padding='same', kernel_initializer=kernel_init)(d)
+    d = InstanceNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+    patch_out = Conv2D(1, (4,4), padding='same', kernel_initializer=kernel_init)(d)
+
+    model = Model(inp, patch_out)
+
+    model.compile(loss='mse', optimizer=Adam(lr=0.0002, beta_1=0.5), loss_weights=[0.5])
     if summary:
         model.summary()
     return model
